74b1e4501b303089eff15279c9551d5aa30a92e7
EmmA <emma.arlanda@viacesi.fr>
2021-02-11 13:27:44 +0100

    BDD test

---
+++ BDD_station_meteo/crebaseTEST.sql
@@ -0,0 +1,7 @@
+CREATE SCHEMA test ;
+CREATE TABLE test.t_rel_test (
+	relid integer,
+	sonid integer,
+	reldatetime timestamp without time zone,
+	relhumidity double precision,
+	reltemperature double precision) ;
